a2545d9c0c1005c96582cd15cd6d276a7b3ab2a5
Alexgry163 <127955264+Alexgry163@users.noreply.github.com>
2023-04-09 18:49:36 +0400

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-
 public class Main {
     public static void main(String[] args) {
 
         int balance = 200;
         int sum = 300;
-        int bonus = sum/100;
-        int x = balance+sum;
+        int bonus = sum / 100;
+        int score = balance + sum;
 
-       if (sum<1000) {bonus = 0;}
-
-
-        System.out.println("Итоговый счет:"+ x);
-        System.out.println("Количество бонусных рублей:"+ bonus);
+        if (sum < 1000) {
+            bonus = 0;
+        }
+        System.out.println("Итоговый счет:" + score);
+        System.out.println("Количество бонусных рублей:" + bonus);
     }
 }
